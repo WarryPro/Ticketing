590de048689738de933c7f16ca50954232a960e5
dany <dannyfr.03@gmail.com>
2019-04-25 22:51:05 +0200

    ajoutés popper, jquery et bootstrap au projet

--- Ticketing/assets/js/app.js
+++ Ticketing/assets/js/app.js
@@ -5,10 +5,15 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single scss file (app.scss in this case)
-require('../scss/app.scss');
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
+import $ from 'jquery';
+import 'bootstrap';
+
+// uncomment to support legacy code
+global.$ = $;
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
