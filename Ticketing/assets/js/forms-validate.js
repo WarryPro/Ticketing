f3c8a3d8e06151039f64d9c3179b8292120adcbb
dany <dannyfr.03@gmail.com>
2019-07-26 00:53:38 +0200

    début details de la commande

--- Ticketing/assets/js/forms-validate.js
+++ Ticketing/assets/js/forms-validate.js
@@ -1,1 +1,28 @@
 // Valider date et heure
+
+
+
+
+// details commande
+function getTypebillet() {
+    const formCard = document.getElementById("form-card");
+
+    formCard.addEventListener("change", (e) => {
+
+        if(e.target.type === "radio") {
+
+            if(e.target.value === '0') {
+                document.getElementById("type-billet").textContent += ' Journée'
+            }else {
+                document.getElementById("type-billet").textContent += ' Demi-journée'
+            }
+
+        }
+        else if(e.target.type === "date") {
+            document.getElementById("date-visite-billet").textContent += e.target.value;
+
+        }
+
+    })
+}
+getTypebillet();
