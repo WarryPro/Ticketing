59ce3b01f69e6fcd0e91e0c58435a3ea42fe5195
dany <dannyfr.03@gmail.com>
2019-09-10 01:03:23 +0200

    Template email et modifs design

--- Ticketing/src/Controller/ConfirmationController.php
+++ Ticketing/src/Controller/ConfirmationController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
-use App\Entity\Buyer;
 
 class ConfirmationController extends AbstractController
 {
